0607bf4e8f4a2138d70602588acd24cb280588f3
OnsOdontotecnica <ons01odontotecnica@gmail.com>
2026-04-20 00:20:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,19 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado');
+const CACHE_NAME = 'ons-odontotecnica-v1';
+const urlsToCache = [
+  '/ONS-Sistema/',
+  '/ONS-Sistema/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return new Response('Offline');
-    })
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
