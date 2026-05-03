d01b3c503f359b0ea3039e173b4650551173298a
OnsOdontotecnica <ons01odontotecnica@gmail.com>
2026-05-02 23:42:57 -0300

    Update service worker cache configuration
    Updated cache name and added Font Awesome CSS to cache.

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = "ons-app-v1";
-
+const CACHE_NAME = 'ons-app-v3';
 const urlsToCache = [
-  "./",
-  "./index.html"
+  '.',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
