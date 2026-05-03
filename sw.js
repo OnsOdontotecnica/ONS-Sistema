a65aa69f0707431bea0a66ee0bad4587356720ed
OnsOdontotecnica <ons01odontotecnica@gmail.com>
2026-05-02 22:50:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = 'ons-admin-v1';
+const CACHE_NAME = "ons-app-v1";
+
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo-icon.png'
+  "./",
+  "./index.html"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
